7daf8e5532758b2e1f50f6af35c87f086454a132
Jailton <jailtonlima1001@gmail.com>
2022-11-02 10:35:00 -0300

    activating the model

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # My applications
+    'learning_logs',
 ]
 
 MIDDLEWARE = [
