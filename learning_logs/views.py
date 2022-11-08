47efe4b42c8f69e2dbd4c068299b4146511859d6
Jailton <jailtonlima1001@gmail.com>
2022-11-08 06:21:41 -0300

    function new_topic()

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -22,3 +25,19 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+	"""add a new subject."""
+	if request.method != 'POST':
+		# No data submitted; create blank form
+		form = TopicForm()
+	else:
+		# Submitted POST data; process the data
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
