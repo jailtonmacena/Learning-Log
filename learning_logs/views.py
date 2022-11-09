0987d49271b102576496e7b9af5b3862ea3ca14a
Jailton <jailtonlima1001@gmail.com>
2022-11-09 06:36:47 -0300

    function new_entry()

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -41,3 +41,25 @@ def new_topic(request):
 
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+	"""Adds a new entry for a particular subject"""
+	topic = Topic.objects.get(id=topic_id)
+
+	if request.method != 'POST':
+		# No data submitted; create blank form
+		form = EntryForm()
+	else:
+		# Submitted POST data; process the data
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return HttpResponseRedirect(reverse('learning_logs:topic',
+				args=[topic_id]))
+
+
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
